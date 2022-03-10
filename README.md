768f40af0d9be79c515b0dd7b3085a2dc0ef4b2a
BossFreind1234 <vincentredtube@gmail.com>
2022-03-10 18:20:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RiceType
+Typing test websit
+[Heres the tutorial](https://youtu.be/2VGUGpZFY5s)
---
+++ src/getText.js
@@ -0,0 +1,12 @@
+export default () => {
+
+  const texts = [
+    "Rice is rice, and I love rice!",
+    + "eeee"
+
+    // add what you want
+  ];
+  
+  return texts[Math.floor(Math.random()*texts.length)];
+
+}
